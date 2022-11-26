95c371cf5fc7f3eb6c144ada22d6da7ece9344ae
suanfazhiwang <2226055846@qq.com>
2022-11-26 10:33:58 +0800

    递归

--- _20221114/_20221114/src/test.java
+++ _20221114/_20221114/src/test.java
@@ -7,9 +7,73 @@ import java.util.Scanner;
  * Date: 2022-11-14
  * Time: 10:04
  * java比较字符串的函数   equals（）   见login方法
+ *
+ * 递归  把原问题分解为小问题，小问题的解决方式和大问题的解决方式是一样的
+ * 1. 自己调用自己
+ * 2. 有一个终止条件（起始条件）
+ * 3. 最难的地方就是如何确定这个递归公式！！！
+ * 循环写和递归写有什么区别？
+ * 递归的好处是：代码少  不好的地方：不好书写
+ * 循环的好处是   容易理解
+ * 递归浪费的空间比较多
+ *
  */
 public class test {
+    public static int fibnacio(int n){
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        return fibnacio(n-1) + fibnacio(n-2);
+    }
 
+    public static void main(String[] args) {
+        System.out.println(fibnacio(1));
+        System.out.println(fibnacio(2));
+        System.out.println(fibnacio(3));
+        System.out.println(fibnacio(40));
+
+
+    }
+    public static int func5(int n){
+        if(n<=9){
+            return n;
+        }
+        return n % 10 + func5(n/10);
+    }
+    public static void func4(int n){
+        if(n <= 9){
+            System.out.println(n);
+            return;
+        }
+        func4(n/10);
+        System.out.println(n % 10);
+
+    }
+    public static int fac(int n){
+        if (n == 1){
+            return 1;
+        }
+        int tmp = n * fac(n-1);
+        return tmp;
+    }
+
+    public static void main2(String[] args) {
+//        System.out.println(fac(5));
+        func4(1234);
+        int i = 1234;
+
+        for (int j = 1; j < 5; j++) {
+            for (int k = 3; k >= j; k--) {
+                i /= 10;
+
+            }
+            System.out.println(i%10);
+            i = 1234;
+        }
+
+
+
+    }
     public static void fib(int n){
         int f1 = 1;
         int f2 = 1;
@@ -67,11 +131,11 @@ public class test {
     }
 
     public static void main1(String[] args) {
-//        double sum = func();
-//        System.out.println(sum);
-//        func2(123);
-//        login();
-//        max3(1,2,3);
+        double sum = func();
+        System.out.println(sum);
+        func2(123);
+        login();
+        max3(1,2,3);
         fib(15);
     }
 }
